72735987eb1b814d02e303f949729e9e8a8da115
Miguel Sanchez <mglsanchez@ucdavis.edu>
2014-05-22 00:28:23 -0700

    Facebook button

---
+++ src/com/example/homestaymanagerandroidapp/MainFragment.java
@@ -0,0 +1,38 @@
+package com.example.homestaymanagerandroidapp;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.facebook.Session;
+import com.facebook.SessionState;
+
+public class MainFragment extends Fragment {
+	@Override
+	public View onCreateView(LayoutInflater inflater, 
+	        ViewGroup container, 
+	        Bundle savedInstanceState) {
+	    View view = inflater.inflate(R.layout.activity_main, container, false);
+	    return view;
+	    
+	}
+	private static final String TAG = "MainFragment";
+	
+	private void onSessionStateChange(Session session, SessionState state, Exception exception) {
+	    if (state.isOpened()) {
+	        Log.i(TAG, "Logged in...");
+	    } else if (state.isClosed()) {
+	        Log.i(TAG, "Logged out...");
+	    }
+	}
+	
+	private Session.StatusCallback callback = new Session.StatusCallback() {
+	    @Override
+	    public void call(Session session, SessionState state, Exception exception) {
+	        onSessionStateChange(session, state, exception);
+	    }
+	};
+}
